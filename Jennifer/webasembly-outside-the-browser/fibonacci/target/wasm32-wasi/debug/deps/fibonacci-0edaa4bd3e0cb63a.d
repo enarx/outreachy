7f09ab8444a169c798b80ea51c0512c7a0434cec
jnyfah <jnyfaah@gmail.com>
2021-10-28 18:31:28 +0100

    update gitignore

--- Jennifer/webasembly-outside-the-browser/fibonacci/target/wasm32-wasi/debug/deps/fibonacci-0edaa4bd3e0cb63a.d
+++
@@ -1,5 +0,0 @@
-/home/jnyfah/Documents/Repo/outreachy/webasembly-outside-the-browser/fibonacci/target/wasm32-wasi/debug/deps/fibonacci-0edaa4bd3e0cb63a.wasm: src/main.rs
-
-/home/jnyfah/Documents/Repo/outreachy/webasembly-outside-the-browser/fibonacci/target/wasm32-wasi/debug/deps/fibonacci-0edaa4bd3e0cb63a.d: src/main.rs
-
-src/main.rs:
