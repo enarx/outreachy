19c50d6c1136901c5fe9153c3dfb8a4ba24e23ea
ydv-sahitya <11sahitya@gmail.com>
2022-04-10 14:30:11 +0530

    added sahitya folder with rust demo on webassembly

---
+++ sahitya/WebAssembly_with_Rust/demo/target/debug/demo.d
@@ -0,0 +1,1 @@
+/home/sahit/Desktop/outreachy/sahitya/WebAssembly_with_Rust/demo/target/debug/demo: /home/sahit/Desktop/outreachy/sahitya/WebAssembly_with_Rust/demo/src/main.rs
---
+++ sahitya/WebAssembly_with_Rust/demo/target/wasm32-wasi/debug/demo.d
@@ -0,0 +1,1 @@
+/home/sahit/Desktop/outreachy/sahitya/WebAssembly_with_Rust/demo/target/wasm32-wasi/debug/demo.wasm: /home/sahit/Desktop/outreachy/sahitya/WebAssembly_with_Rust/demo/src/main.rs
