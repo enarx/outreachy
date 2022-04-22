751f5d8d97933531dabcf510aa63025b60f4f88a
daliseiy <obakunleoluseye@gmail.com>
2022-04-22 06:49:14 +0100

    removed inecessary files

---
+++ seye/fixed_partition_scheme/target/wasm32-wasi/release/deps/fixed_partition_scheme-5e008705d347239f.d
@@ -0,0 +1,5 @@
+/home/daliseiy/Documents/opensource/outreachy/seye/fixed_partition_scheme/target/wasm32-wasi/release/deps/fixed_partition_scheme-5e008705d347239f.wasm: src/main.rs
+
+/home/daliseiy/Documents/opensource/outreachy/seye/fixed_partition_scheme/target/wasm32-wasi/release/deps/fixed_partition_scheme-5e008705d347239f.d: src/main.rs
+
+src/main.rs:
---
+++ seye/fixed_partition_scheme/target/wasm32-wasi/release/fixed_partition_scheme.d
@@ -0,0 +1,1 @@
+/home/daliseiy/Documents/opensource/outreachy/seye/fixed_partition_scheme/target/wasm32-wasi/release/fixed_partition_scheme.wasm: /home/daliseiy/Documents/opensource/outreachy/seye/fixed_partition_scheme/src/main.rs
